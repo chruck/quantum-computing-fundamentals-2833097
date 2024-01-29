2b0d0b9a242980f9bf0732244c3c806982fb43a8
Jas Eckard <jas+git@eckard.com>
2024-01-29 15:40:26 -0500

    feat: created boilerplate for qiskit programs

---
+++ src/01_03_measurement/boilerplate.py
@@ -0,0 +1,11 @@
+# Import Qiskit into current namespace:
+from qiskit import *
+
+# Use Qiskit namespace:
+#import qiskit
+
+# To plot histograms:
+from qiskit.tools.visualization import plot_histogram
+
+# I think this is only for Jupyter:
+%matplotlib inline
